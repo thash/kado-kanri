8860e02216c2374ea68172a80158bbd40eacbda6
T.H.@Centillion <hashimoto@centillion.co.jp>
2011-05-21 00:18:02 +0900

    abstraction has finished. now it comes to usage problem

--- kado-kanri.rb
+++ kado-kanri.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'pp'
 
 
+
 class WorkProject
 	def initialize(id, client, pj_name)
 		@id      = id     
@@ -20,9 +21,9 @@ class PrivacyUtil
 	end
 
 
-    ### usage ###
-    # 	mail = json_hash["mail"]
-    # 	password = json_hash["password"]
+	### usage ###
+	# 	mail = json_hash["mail"]
+	# 	password = json_hash["password"]
 	def make_hash_from_json
 		if File.exist?(@propaties_file)
 			f = File.open(@propaties_file)
@@ -49,26 +50,25 @@ def select_sheet(book, year, month)
 end
 
 def find_row(sheet, pj)
+
 	cells = sheet.cells
+
 	### row scan...
 	row_num = 1
 	catch(:last){
 		sheet.UsedRange.Rows.each do |row|
-		record = []
 
 		### column scan...
-		col_num = 1
 		row.Columns.each do |cell|
-			print "DEBUG: "; puts cell.Address
+			#print "DEBUG: "; puts cell.Address
 
 			### check Project code...
 			if cell.Value == pj
-				puts "XXXXXXXXX HIT XXXXXXXXXXX"
+				puts "XXXXXXXXX HIT - ROW XXXXXXXXX"
 				@target_row_num = row_num
 				puts "TARGET_ROW::: " + @target_row_num.to_s
 				throw :last
 			end
-		col_num += 1
 		end
 		row_num += 1
 		end
@@ -78,46 +78,79 @@ def find_row(sheet, pj)
 
 end
 
-#def insert_hour(sheet, day, pj, hour)
-def insert_hour(sheet, pj, hour)
+def find_col(sheet, day)
+
+	cells = sheet.cells
+
+	### column scan...
+	col_num = 1
+	catch(:last){
+		### FORMAT: date need to be placed in 4th row
+		sheet.UsedRange.Rows[4].Columns.each do |col|
+
+		col.Rows.each do |cell|
+			#print "DEBUG: "; puts cell.Address
+
+	#puts "DEBUG: " + "day: " + day
+	#puts "DEBUG: " + "cell.Value: " + cell.Value.to_i.to_s
+			### check Project code...
+			if cell.Value.to_i.to_s == day
+				puts "XXXXXXXXX HIT - COL XXXXXXXXX"
+				@target_col_num = col_num
+				puts "TARGET_COL::: " + @target_col_num.to_s
+				throw :last
+			end
+			col_num += 1
+		end
+		end
+	}
+
+	return @target_col_num
+end
+
+def insert_hour(sheet, day, pj, hour)
 
 	@target_row_num = find_row(sheet, pj)
-	#TODO: @target_col_num = find_col(sheet, day)
+	@target_col_num = find_col(sheet, day)
 
 	cells = sheet.cells
 	puts "CHANGE CELL::: " +  "(" + @target_row_num.to_s + "," + @target_col_num.to_s +  ")"
 	print "before: "
-	pp cells.item(@target_row_num, 7).value
+	pp cells.item(@target_row_num, @target_col_num).value
 
-	cells.item(@target_row_num, 7).value = hour.to_s
+	cells.item(@target_row_num, @target_col_num).value = hour.to_s
 
 	print "after: "
-	pp cells.item(@target_row_num, 7).value
-	#cells.item(1, 7).value = hour.to_s
+	pp cells.item(@target_row_num, @target_col_num).value
 end
 
 
+
+### MAIN PROCESS ###
 class EditExcel
-
-	today_full = Date.today
-	today = today_full.day
-	print "today's [day] is..." + today.to_s + "\n"
-	print "target [day] is..." + "NOTDEFINED" + "\n"
-
-
-	excel = WIN32OLE.new('Excel.Application')
-	excel.visible = true
 
 	s = PrivacyUtil.new(ARGV)
 	res = s.make_hash_from_json
 	path = res["path_test"]
+	arg_year = res["year"] #TODO: Date.today.year
+	arg_month = res["month"] #TODO: Date.today.month
+	arg_day = res["day"] # arg_day = Date.today.day
+	arg_pj = res["pj"]
+	arg_hour = res["hour"]
+
 	puts path
+	print "TARGET [pj]  is..." + arg_pj.to_s + "\n"
+	print "TARGET [day] is..." + arg_day.to_s + "\n"
+
+	excel = WIN32OLE.new('Excel.Application')
+	excel.visible = true
+
 	#book = excel.Workbooks.Open(excel.GetOpenFilename)
 	book = excel.Workbooks.Open(path)
 
-	sheet = select_sheet(book, "2011", "04")
+	sheet = select_sheet(book, arg_year, arg_month)
 
-	insert_hour(sheet, "E100802T01", 3)
+	insert_hour(sheet, arg_day, arg_pj, arg_hour)
 
 	#book.close(false)
 	#excel.quit
