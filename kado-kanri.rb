bf774c1cb7958d0a5d7e4bc32241bbe5657cfa10
T.H.@Centillion <hashimoto@centillion.co.jp>
2011-05-20 23:35:03 +0900

    seach PJ ID and insert hour into target row

--- kado-kanri.rb
+++ kado-kanri.rb
@@ -19,16 +19,16 @@ class PrivacyUtil
 		@propaties_file = propaties_file[0]
 	end
 
+
+    ### usage ###
+    # 	mail = json_hash["mail"]
+    # 	password = json_hash["password"]
 	def make_hash_from_json
 		if File.exist?(@propaties_file)
 			f = File.open(@propaties_file)
 			json_hash = JSON.parse(f.read)
 
 			return json_hash
-
-			### usage
-			# 	mail = json_hash["mail"]
-			# 	password = json_hash["password"]
 
 		else 
 			puts "ERROR: please put #{@propaties_file} in your dir"
@@ -37,22 +37,67 @@ class PrivacyUtil
 	end
 end
 
+
+def insert_hour(sheet, pj, hour) #{{{
+
+	cells = sheet.cells
+	### row scan...
+	row_num = 1
+	catch(:last){
+		sheet.UsedRange.Rows.each do |row|
+		record = []
+
+		### column scan...
+		col_num = 1
+		row.Columns.each do |cell|
+			puts cell.Address
+
+			### check Project code...
+			if cell.Value == pj
+				puts "XXXXXXXXX HIT XXXXXXXXXXX"
+				#@target_row = row.Address
+				#puts "TARGET::: " + @target_row
+				@target_row_num = row_num
+				puts "TARGET_ROW::: " + @target_row_num.to_s
+				throw :last
+			end
+		col_num += 1
+		end
+		row_num += 1
+		end
+	}
+
+	#cells.item(target_row, 7).value = hour.to_s
+	puts "CHANGE CELL::: " +  "(" + @target_row_num.to_s + "," + @target_col_num.to_s +  ")"
+	print "before: "
+	pp cells.item(@target_row_num, 7).value
+
+	cells.item(@target_row_num, 7).value = hour.to_s
+
+	print "after: "
+	pp cells.item(@target_row_num, 7).value
+	#cells.item(1, 7).value = hour.to_s
+end #}}}
+
+
 class ShowExcel
 
-		today_full = Date.today
-		today = today_full.day
-		print "today's [day] is..." + today.to_s + "\n"
-		print "target [day] is..." + "NOTDEFINED" + "\n"
+	today_full = Date.today
+	today = today_full.day
+	print "today's [day] is..." + today.to_s + "\n"
+	print "target [day] is..." + "NOTDEFINED" + "\n"
 
 
 	excel = WIN32OLE.new('Excel.Application')
+	excel.visible = true
 
-    s = PrivacyUtil.new(ARGV)
+	s = PrivacyUtil.new(ARGV)
 	res = s.make_hash_from_json
-	path = res["path_test_jp"]
+	path = res["path_test"]
 	puts path
-	#book = excel.Workbooks.Open(app.GetOpenFilename)
+	#book = excel.Workbooks.Open(excel.GetOpenFilename)
 	book = excel.Workbooks.Open(path)
+
 
 	flag = 0
 	book.WorkSheets.each do |sheet|
@@ -60,51 +105,10 @@ class ShowExcel
 			puts sheet.Name
 			puts ""
 
-			### row scan...
-			sheet.UsedRange.Rows.each do |row|
-				record = []
+			insert_hour(sheet, "E100802T01", 3)
 
-				### column scan...
-				row.Columns.each do |cell|
-					puts cell.Address
-					
-					### check Project code...
-					if cell.Value == "E100802T01"
-							puts "XXXXXXXXXXXXXXXXXXXXXXXXXX"
-							target_row = row.Address
-							puts target_row
-							exit
-						flag = 1
-						record << cell.Value
-					#	if cell.Value == "ID"
-					#		tmpAd = cell.Address 
-					#		puts tmpAd
-					#	end
-					end
-				end
-			#	puts record.join(",")
-			end
 		end
 	end
-
-#	#使っているワークシート範囲を一行ずつ取り出す
-#	for row in book.ActiveSheet.UsedRange.Rows do
-#		#取り出した行から、セルを一つづつ取り出す
-#		for cell in row.Columns do
-#			p cell.Address
-#			p (cell.Value.class) == String ? \
-#				cell.Value.encode("cp932") : \
-#				cell.Value
-#
-#			p '-------'
-#		end
-#	end
-
-	book.close(false)
-	app.quit
+	#book.close(false)
+	#excel.quit
 end
-
-# s = PrivacyUtil.new(ARGV)
-# puts s
-# pp s
-# puts s.make_hash_from_json
